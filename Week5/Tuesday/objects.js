3959db2036a6491d231a9145417b7021beea7a74
Veronica <Veronica@digitalcrafts.com>
2018-07-24 15:31:24 -0500

    solutions

---
+++ Week5/Tuesday/objects.js
@@ -0,0 +1,91 @@
+// var course = new Object();
+
+// var course = {};
+
+// course.title = "digi crafts";
+// course.instructor = "Veronica";
+
+// console.log(course);
+// course1 = new Object();
+
+
+// function Course(title, instructor, level, published, views){
+//     this.title = title;
+//     this.instructor = instructor;
+//     this.level = level;
+//     this.published = published;
+//     this.views = views;  
+// }
+
+// var course1 = new Course("dig crafts", "Veronica", 1, true, 155);
+// var course2 = new Course("dig crafts", "Azam", 1, true, 155);
+// var course3 = new Course("dig crafts", "Veronica", 1, true, 155);
+// var course4 = new Course("dig crafts", "Veronica", 1, true, 155);
+
+// var myArray = [course1, course2, course3, course4];
+
+// console.log(myArray);
+
+// 
+
+// for (let i = 0; i<= 10; i++){
+
+// }
+
+// let myArray = [1,2,3,4]
+
+// myArray.forEach(function(arrayIndex){
+//     console.log(arrayIndex);
+// })
+
+// var person = [1, 2, 4, 5];
+
+// for (var attribute in person) {
+//     var value = person[attribute];
+//     console.log(`${attribute}: ${value}`);
+// }
+
+
+// var i = 0;
+
+// function someFunc(){
+ 
+//     var i = 100;
+
+//     for (var i = 10; i<=50; i*=2)
+//     {
+//       console.log(i);
+//     }
+
+//     console.log("inner function", i)
+// }
+
+// someFunc();
+// console.log("outer function", i);
+
+// var X = function(j) {
+//     this.i = 0; //setting default value
+//     this.j = j;
+
+//     this.getJ = function(){
+//         return this.j;
+//     };
+// }
+
+// var x1 = new X(3);
+// var x2 = new X(4);
+
+// console
+
+// function Character (x, y) {
+//     this.x = x;
+//     this.y = y;
+    
+//     console.log('character');
+// }
+
+// var myChar = new Character(2, 3);
+
+console.log(p);
+
+let p = 34;
