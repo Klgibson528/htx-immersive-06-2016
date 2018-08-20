e9880c6e284b0ac5713fb7f893aa5bcaa252d411
Veronica <Veronica@digitalcrafts.com>
2018-08-20 17:48:44 -0500

    class sql

--- Week9/Monday/class.sql
+++ Week9/Monday/class.sql
@@ -82,4 +82,74 @@
 -- );
 
 
-UPDATE student2 SET name = 'Matt Fisher' WHERE id = 2;
+-- UPDATE student2 SET name = 'Matt Fisher' WHERE name = 'Eric';
+-- UPDATE student2 SET points = 8;
+
+-- UPDATE student2 SET points = 2, graduated = TRUE 
+--   WHERE id = 1;
+
+-- DELETE FROM student2 WHERE id = 4;
+
+-- CREATE TABLE student3 (
+--   id SERIAL NOT NULL PRIMARY KEY,
+--   name VARCHAR,
+--   github VARCHAR,
+--   points INTEGER DEFAULT 0,
+--   start_date DATE,
+--   graduated BOOLEAN DEFAULT FALSE
+-- );
+
+-- INSERT INTO student3 (name) VALUES (
+--   'Paul'
+-- );
+
+-- CREATE TABLE student4 (
+--   id SERIAL NOT NULL PRIMARY KEY,
+--   name varchar,
+--   github varchar UNIQUE NOT NULL,
+--   points integer DEFAULT 0 CHECK (points >= 0),
+--   start_date date NOT NULL,
+--   graduated boolean DEFAULT FALSE
+-- );
+
+-- INSERT INTO student4 (name, github, points,  start_date) VALUES (
+--   'Paul', 'github35', 2, '2017-6-6'
+-- );
+
+-- SELECT * FROM student2;
+
+-- SELECT name, points FROM student2;
+
+-- SELECT * FROM student2 WHERE name = 'Matt Fisher';
+
+-- SELECT * FROM student2 WHERE points > 7 and graduated = TRUE;
+
+-- SELECT * FROM student2 WHERE name ilike '%mat%';
+
+-- SELECT * FROM student WHERE website is NULL;
+
+-- SELECT * FROM student2 WHERE start_date > '2018-01-01';
+
+-- ALTER TABLE student
+-- ADD city varchar(30);
+
+-- ALTER TABLE student
+-- DROP COLUMN city
+
+-- ALTER TABLE student
+-- ALTER COLUMN city integer
+
+-- SELECT COUNT(*) FROM student2;
+
+-- SELECT COUNT(github) FROM student2;
+
+-- SELECT SUM(points) FROM student2 WHERE graduated = FALSE;
+-- SELECT MIN(points) FROM student2 WHERE graduated = FALSE;
+
+-- SELECT start_date, sum(points) FROM GROUP BY start_date;
+
+-- SELECT * FROM student2 ORDER BY name DESC;
+
+-- SELECT * FROM student2 ORDER BY name DESC LIMIT 2;
+
+SELECT * FROM student2 ORDER BY points LIMIT 5 OFFSET 5;
